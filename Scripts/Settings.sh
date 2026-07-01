01e652590ea0b29288b6b0ead4dbbcd564bcf91b
47533 <52405936+47533@users.noreply.github.com>
2026-07-01 22:17:05 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,97 +1,5 @@
 #!/bin/bash
 
-
-# skb 回收
-function enable_skb_recycler() {
-  if [ -f "$1" ]; then
-    cat >> "$1" <<EOF
-
-CONFIG_KERNEL_SKB_RECYCLER=y
-CONFIG_KERNEL_SKB_RECYCLER_MULTI_CPU=y
-EOF
-  fi
-}
-
-########################################
-# 固定 kernel 6.18 新增 perf 选项
-########################################
-
-# function pin_arm_perf_kernel_config() {
-#   local target
-#   target=$(grep -m 1 -oP '^CONFIG_TARGET_qualcommax_\K[[:alnum:]_]+(?=\=y)' "$GITHUB_WORKSPACE/Config/${WRT_CONFIG}.txt")
-
-#   local kernel_config="target/linux/qualcommax/${target}/config-default"
-#   if [ ! -f "$kernel_config" ]; then
-#     echo "skip kernel perf config: $kernel_config not found"
-#    return 0
-#   fi
-
-#   cat >> "$kernel_config" <<'EOF'
-
-# # Kernel 6.18 eBPF/BTF perf dependencies
-# # CONFIG_ARM64_BRBE is not set
-# # CONFIG_ARM_CCI_PMU is not set
-# # CONFIG_ARM_CCN is not set
-# # CONFIG_ARM_CMN is not set
-# # CONFIG_ARM_NI is not set
-# # CONFIG_ARM_SMMU_V3_PMU is not set
-# # CONFIG_ARM_DSU_PMU is not set
-# # CONFIG_ARM_SPE_PMU is not set
-# EOF
-# }
-
-########################################
-# 修改内核大小
-########################################
-
-function set_kernel_size() {
-
-  for file in target/linux/qualcommax/image/*.mk; do
-    sed -i 's/KERNEL_SIZE := [0-9]*k/KERNEL_SIZE := 12288k/g' "$file"
-  done
-
-}
-
-########################################
-# 生成最终 .config
-########################################
-
-function generate_config() {
-
-  config_file=".config"
-
-  cat "$GITHUB_WORKSPACE/Config/${WRT_CONFIG}.txt" \
-      "$GITHUB_WORKSPACE/Config/GENERAL.txt" > "$config_file"
-
-  local target=$(echo "$WRT_ARCH" | cut -d'_' -f2)
-
-  # 删除 WIFI
-  if [[ "$WRT_CONFIG" == *"NOWIFI"* ]]; then
-    remove_wifi "$target"
-  fi
-
-  # skb recycler
-  enable_skb_recycler "$config_file"
-
-  # 内核大小
-  set_kernel_size
-
-  # kernel 6.18 perf config
-  pin_arm_perf_kernel_config
-
-}
-
-########################################
-# 执行生成 config
-########################################
-
-generate_config
-
-########################################
-# Luci / 系统修改
-########################################
-
-
 #移除luci-app-attendedsysupgrade
 sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改默认主题
@@ -112,11 +20,7 @@ elif [ -f "$WIFI_UC" ]; then
 	#修改WIFI名称
 	sed -i "s/ssid='.*'/ssid='$WRT_SSID'/g" $WIFI_UC
 	#修改WIFI密码
-	sed -i "s/key='.*'/key='$WRT_WORD'/g" $WIFI_UC
-	#修改WIFI地区
-	sed -i "s/country='.*'/country='CN'/g" $WIFI_UC
-	#修改WIFI加密
-	sed -i "s/encryption='.*'/encryption='psk2+ccmp'/g" $WIFI_UC
+	sed -i "s/key='.*'/key='$WRT_WORD'/g" $WIFI_UC	
 fi
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
